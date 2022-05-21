256be87651221b4aebeac8ef711cfd64be48fccd
Evgenii Zhuravlev <vijey1989@mail.ru>
2022-05-22 00:04:43 +0500

    2.2 with stackView ready to check

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -12,24 +12,57 @@ class FeedViewController: UIViewController {
     //экземпляр структуры Post
     let post = Post(title: "Заголовок поста")
 
+    //stackView
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.backgroundColor = .green
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        return stackView
+    }()
+
+    //кнопки
+    let button1: UIButton = {
+        let button = UIButton()
+        //button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Just button", for: .normal)
+        button.backgroundColor = .gray
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        return button
+    }()
+
+    let button2: UIButton = {
+        let button = UIButton()
+        //button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Just button", for: .normal)
+        button.backgroundColor = .systemOrange
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .blue
         self.title = "User feed"
-        createButton()
+        view.addSubview(stackView)
+        setLayots()
         // Do any additional setup after loading the view.
+        stackView.addArrangedSubview(button1)
+        stackView.addArrangedSubview(button2)
         print(#function)
     }
 
-    //создаём кнопку
-    private func createButton() {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
-        button.center = view.center
-        button.setTitle("Open", for: .normal)
-        button.backgroundColor = .red
-        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
-        view.addSubview(button)
-        print(#function)
+    //настройка ограничений
+    private func setLayots() {
+        NSLayoutConstraint.activate([
+            //stackView
+            stackView.topAnchor.constraint(equalTo: super.view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: super.view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: super.view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: super.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 
     //действия при нажатии на кнопку
